1199787908b9f4b4d06b89ab25a9ca62ba9ee42e
Tavera100 <tavera100@gmail.com>
2024-09-23 11:40:37 -0500

    Create Tavera100.py
    Aprendiendo conceptos básicos del lenguaje Pyhton

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Tavera100.py
@@ -0,0 +1,44 @@
+# www.python.org
+
+# Esto es un comentario de una sola linea, se realiza con #
+
+""" 
+EJERCICIO:
+ - Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado.
+ - Representa las diferentes sintaxis que existen de crear comentarios
+  en el lenguaje (en una línea, varias...).
+ - Crea una variable (y una constante si el lenguaje lo soporta).
+ - Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+ - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ 
+ -OJO: se pueden usar dentro del bloque las comillas que no hayamos usado para abrir el bloque
+ -Si usasemos las mismas cerrariamos el comentario sin querer
+ -Por ejemplo en este puedo usar las simples sin problema '''''''
+"""
+
+'''
+Aqui puedo usar comillas dobles si quiero """"""""""""
+'''
+
+"""
+Comentario de varias lineas
+"""
+
+# Tipos de datos primitivos
+CONSTANTE = "esta es sólo la manera de escribir su nombre por convencion, Python no tiene constantes"
+Cadena o String = "esto es una string"
+Entero = 5
+Flotante = 4.2
+Complejo = 4j
+Booleano = False
+Booleano = True
+Lista = [1,dos,3]
+Tupla = (uno,2,tres)
+Diccionario = {"valor1":1,"valor2":"dos"}
+
+
+
+# Imprimiendo el nombre del lenguaje
+print("¡Hola, Python!")
